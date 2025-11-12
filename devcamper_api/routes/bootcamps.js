ce50e4861e5422bfafd6394d0e70afc353fb722d
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-11-12 03:57:48 +0100

    trying out expressjs

--- devcamper_api/routes/bootcamps.js
+++ devcamper_api/routes/bootcamps.js
@@ -1,35 +1,19 @@
 const express = require("express");
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamps,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  // res.send("<h1>Hello from Express</h1>");
-  // res.json({ name: "Blaise" });
-  // res.sendStatus(400)
-  // res.status(400).json({ success: false });
-  // res.status(200).json({ success: true, data: [{ id: 1 }, { id: 2 }] });
-  res.status(200).json({ success: true, message: "Show all bootcamps" });
-});
-
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Show bootcamp ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ success: true, message: "Create new bootcamp" });
-});
-
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Update bootcamp ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Delete bootcamp ${req.params.id}` });
-});
+router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamps)
+  .delete(deleteBootcamp);
 
 module.exports = router;
