239ac6ada47d704fa40a74d43135e78f4eff7b62
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-12-09 14:56:44 +0100

    models

--- devcamper_api/controllers/bootcamps.js
+++ devcamper_api/controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require("../models/Bootcamp");
+
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
@@ -19,8 +21,13 @@ exports.getSingleBootcamp = (req, res, next) => {
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+exports.createBootcamp = async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
 };
 
 // @desc      Update bootcamp
--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -17,6 +17,9 @@ const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // app.use(logger, urlStuff);
 
 // Dev login middleware
