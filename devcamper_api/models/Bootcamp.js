9603ed8b63a129c1985d18f8e0c3c7b991259272
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-12-18 15:27:22 +0100

    Create slug from bootcamp name

--- devcamper_api/models/Bootcamp.js
+++ devcamper_api/models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -96,6 +97,14 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create bootcamp slug from name
+BootcampSchema.pre("save", function (next) {
+  // console.log("Slugify ran", this.name);
+
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
